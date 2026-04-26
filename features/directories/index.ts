50ba8efd8dde97c6df999f69d7ea05b17661757a
huntechri <huntechri162378@gmail.com>
2026-04-26 18:58:36 +0300

    refactor(directory): export entity sheet shell

--- features/directories/index.ts
+++ features/directories/index.ts
@@ -1,3 +1,4 @@
 export { DirectoryListScreen } from '@/features/directories/components/directory-list-screen';
 export type { DirectoryListAdapter } from '@/features/directories/components/directory-list-screen';
+export { DirectoryEntitySheetShell } from '@/features/directories/components/directory-entity-sheet-shell';
 export * from './hooks/use-directory-sheet-state';
