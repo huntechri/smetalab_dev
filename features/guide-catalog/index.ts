0585e46d42f335c322340afbd733b369f429dd44
huntechri <huntechri162378@gmail.com>
2026-04-30 14:46:59 +0300

    refactor: keep guide catalog compatibility barrel

--- features/guide-catalog/index.ts
+++ features/guide-catalog/index.ts
@@ -1,9 +1,1 @@
-export * from "./components/CatalogScreenShell";
-export * from "./components/CatalogScreenFallback";
-export * from "./components/CatalogTableWrapper";
-export * from "./components/CatalogFeatureTableWrapper";
-export * from "./components/CatalogToolbar";
-export * from "./hooks/use-catalog-edit-field-change";
-export * from "./hooks/use-catalog-screen-view-state";
-export * from "./types";
-
+export * from "@/features/_shared/guide-catalog";
