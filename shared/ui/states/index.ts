4fbca78eb9e821fb32b5b7975a7c1e6741c519a2
huntechri <huntechri162378@gmail.com>
2026-05-02 19:33:22 +0300

    refactor(ui): export no-results state

--- shared/ui/states/index.ts
+++ shared/ui/states/index.ts
@@ -2,4 +2,5 @@ export * from './EmptyState';
 export * from './ErrorState';
 export * from './ForbiddenState';
 export * from './LoadingState';
+export * from './NoResultsState';
 export * from './StateShell';
